88c6a5c4b2fb7b84f5f0bf0a910a3d9ee1789f94
Emma24968 <emnabs12@gmail.com>
2026-01-31 03:12:36 +0100

    Added responsiveness to the menu icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets";
 
 export const NavBar = ({ theme, setTheme }) => {
+  const [sideBarOpen, setSideBarOpen] = useState(false);
   return (
     <div className="flex justify-between items-center px-4 sm:px-12 lg:px-24 xl:px-40 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70">
       <img
@@ -9,9 +10,19 @@ export const NavBar = ({ theme, setTheme }) => {
         className="w-32 sm:w-40"
         alt="logo"
       />
-      <div className="text-gray-700 dark:text-white sm:text-sm max-sm:w-60 max-sm:pl-10 max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all">
-      <img src={assets.close_icon} alt="close icon" className="absolute w-5 right-4 sm:hidden" />
-        <a href="#" className="sm:hover:border-b">
+      <div
+        className={`text-gray-700 dark:text-white sm:text-sm ${!sideBarOpen ? "max-sm:w-0 overflow-hidden" : " max-sm:w-60 max-sm:pl-10"} max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}
+      >
+        <img
+          src={assets.close_icon}
+          alt="close icon"
+          className="absolute w-5 right-4 sm:hidden" onClick={()=>setSideBarOpen(false)}
+        />
+        <a
+          href="#"
+          className="sm:hover:border-b"
+          onClick={() => setSideBarOpen(false)}
+        >
           Home
         </a>
         <a href="#" className="sm:hover:border-b">
@@ -24,7 +35,13 @@ export const NavBar = ({ theme, setTheme }) => {
           Contact Us
         </a>
       </div>
-      <div className="div">
+      <div className="flex items-center gap-2 sm:gap-4">
+        <img
+          src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon}
+          onClick={() => setSideBarOpen(true)}
+          alt="menu icon"
+          className="sm:hidden w-8"
+        />
         <a
           href="#"
           className="text-sm max-sm:hidden flex items-center gap-2 bg-primary text-white px-6 py-2 rounded-full cursor-pointer transition-all"
