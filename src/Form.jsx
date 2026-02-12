08a294b7133e31ec40b32050bde4f9812ee101f9
Emma24968 <emnabs12@gmail.com>
2026-02-12 14:35:46 +0100

    Created the form section

--- src/Form.jsx
+++ src/Form.jsx
@@ -2,6 +2,34 @@ import React from 'react'
 
 export default function Form() {
   return (
-    <div>Form</div>
+
+    <form className="max-w-xl mx-auto p-6 space-y-4">
+
+      <div className="flex flex-col sm:flex-row gap-4">
+        <input
+          type="text"
+          placeholder="Your Name"
+          className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+        />
+
+        <input
+          type="email"
+          placeholder="Your Email"
+          className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+        />
+      </div>
+
+      <textarea
+        placeholder="Your Message"
+        rows="5"
+        className="w-full border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+      ></textarea>
+      <button
+        type="submit"
+        className=" bg-blue-700 text-white p-3 rounded-lg hover:opacity-90 transition"
+      >
+        Submit
+      </button>
+    </form>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import assets from '../assets/assets'
+import Form from '../Form'
 
 export default function Contact({title,image}) {
   return (
-<div >
+<div className='flex flex-col items-center  gap-6 py-20 px-4 text-center w-full' >
           <h3 className='font-bold text-3xl'>Reach out to us</h3>
           <p>Ready to grow your brand? Let's connect and build something<br />exceptional together. </p>
+          <Form />
         </div>  )
 }
