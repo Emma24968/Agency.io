23e74bff3120cdcc5f747af47bc03fd4b8a602c2
Emma24968 <emnabs12@gmail.com>
2026-02-12 13:03:37 +0100

    Created the teams component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,10 +8,28 @@ export default function Hero() {
         <img src={assets.group_profile} alt="" width={70} />
         <span className="font-medium text-xs">Trusted by 10k+ people</span>
       </button>
-      <h1 className="font-bold text-5xl">Turning imagination into<p><span className="bg-gradient-to-r from-[#5044e5] to-[#4d8cea] bg-clip-text text-transparent" >digital </span>impact</p></h1>
-      <h5>Creating meaning connections and turning big ideas <p>into interactive digital experiences.</p></h5>
-      <img src={assets.bgImage1} alt="gradient color" className="absolute -top-60  mr-1 " />
-      <img src={assets.hero_img} alt="hero image" width={800} className="z-10" />
+      <h1 className="font-bold text-5xl">
+        Turning imagination into
+        <p>
+          <span className="bg-gradient-to-r from-[#5044e5] to-[#4d8cea] bg-clip-text text-transparent">
+            digital{" "}
+          </span>
+          impact
+        </p>
+      </h1>
+      <h5>
+        Creating meaning connections and turning big ideas{" "}
+        <p>into interactive digital experiences.</p>
+      </h5>
+      <div className="absolute left-100 top-70 -z-50">
+        <img src={assets.bgImage1} alt="gradient color" className="absolute -top-60  mr-1 " />
+      </div>
+      <img
+        src={assets.hero_img}
+        alt="hero image"
+        width={800}
+        className="z-10"
+      />
     </div>
   );
 }
---
+++ src/components/Team.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Team() {
+  return (
+    <div>
+        <h3>Meet the Team</h3>
+        <p>A passionate team of digial experts dedicated to your brand's success</p>
+    </div>
+  )
+}
