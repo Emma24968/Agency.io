307bfb662e4ceb25afec440f057e767d919ae369
Emma24968 <emnabs12@gmail.com>
2026-02-12 13:51:34 +0100

    Created the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,7 @@ import assets from '../assets/assets'
 export default function Contact({title,image}) {
   return (
 <div >
-          
+          <h3 className='font-bold text-3xl'>Reach out to us</h3>
+          <p>Ready to grow your brand? Let's connect and build something<br />exceptional together. </p>
         </div>  )
 }
